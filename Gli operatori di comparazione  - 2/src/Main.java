ec8fe4242924bcd5f6f3cef48f6629f1071dc647
Bruno <bruno1999@hotmail.it>
2023-09-26 14:41:25 +0200

    correzione esercizio operatori di comparazione - 2

--- Gli operatori di comparazione  - 2/src/Main.java
+++ Gli operatori di comparazione  - 2/src/Main.java
@@ -1,13 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         char x = 'b';
-        char y = 'c';
+        char y = 'b';
         System.out.println("x = " + x);
         System.out.println("y = " + y);
         System.out.println("Le lettere sono uguali: " + equal(x,y));
     }
     public static boolean equal(char x, char y) {
-        boolean z = x == y;
-        return z;
+        return x == y;
     }
 }
