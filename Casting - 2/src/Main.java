a04ffe88ae8cf72379111e40bff5bb25864096e3
Bruno <bruno1999@hotmail.it>
2023-09-27 09:29:38 +0200

    add folders Casting-2

---
+++ Casting - 2/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        char a = 'b';
+        char b = 'a';
+
+        int sommaAscii = (int)a + (int)b;
+
+        System.out.println("La somma dei due caratteri in ASCII è : " + sommaAscii);
+    }
+}
