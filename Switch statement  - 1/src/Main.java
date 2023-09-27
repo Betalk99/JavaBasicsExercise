ddb56e0041a8f358a5eb980fcbf2477e38570fa5
Bruno <bruno1999@hotmail.it>
2023-09-27 11:26:19 +0200

    add exercise Switch statament - 1

---
+++ Switch statement  - 1/src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        int numSet = 2;
+        
+        System.out.println(checkDay(numSet));
+    }
+    public static String checkDay(int x){
+        String result;
+        switch (x){
+            case 1 :
+                result = "Lunedì";
+                break;
+            case 2 :
+                result = "Martedì";
+                break;
+            case 3 :
+                result = "Mercoledì";
+                break;
+            case 4 :
+                result = "Giovedì";
+                break;
+            case 5 :
+                result = "Venerdì";
+                break;
+            case 6 :
+                result = "Sabato";
+                break;
+            case 7 :
+                result = "Domenica";
+                break;
+            default:
+                result = "Errore";
+                break;
+        }
+    return result;
+
+    }
+}
