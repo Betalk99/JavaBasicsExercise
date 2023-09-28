a640e16f9bdfd9497badab527a782518ca49db3c
Bruno <bruno1999@hotmail.it>
2023-09-28 14:21:17 +0200

    add array 1

---
+++ Gli array - 1/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arrayNum = creaERiempiNum();
+
+        // Stampiamo l'array di numeri
+        for (int numero : arrayNum) {
+            System.out.print(numero + " ");
+        }
+    }
+    public static int[] creaERiempiNum() {
+        int[] array = new int[10];
+        int somma = 0;
+        // Riempimento dell'array con il valore specifico
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i+1;
+            somma = somma + array[i];
+        }
+        System.out.println("La somma è : "+ somma);
+        return array;
+    }
+
+
+}
