74ae6d49d33f40b161badbb54eea7930dae34a52
Bruno <bruno1999@hotmail.it>
2023-09-28 19:54:33 +0200

    add exercise array multidimensionali - 2

---
+++ Gli Array multidimensionali - 2/src/Main.java
@@ -0,0 +1,47 @@
+/*Scrivere un programma che contenga un metodo che permette di inizializzare una matrice e di
+scambiare le sue righe e le sue colonne stampandola a video. Matrice di partenza
+
+        1 2 3
+        4 5 6
+        Matrice risultato
+
+        1 4
+        2 5
+        3 6*/
+
+public class Main {
+    public static void main(String[] args) {
+        int [][] num = {{1,2,3},{4,5,6}};
+        int [][] numScamb = scambPosiz(num);
+
+        stampaArray(numScamb);
+
+    }
+    //invertire il numero di colonne con le righe
+    public static int [][] scambPosiz(int [][] arr){
+        int righe = arr.length;
+        int colonne = arr[0].length;
+
+        int [][] arrScam = new int[colonne][righe];
+
+        for(int i = 0; i < righe; i++){
+            for(int j = 0; j < colonne;j++){
+                arrScam[j][i] = arr[i][j];
+            }
+
+        }
+
+    return arrScam;
+    }
+    // stampare array
+    public static void stampaArray(int [][] arr){
+        for(int i = 0 ; i < arr.length ; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(" " + arr[i][j]);
+            }
+            System.out.println(" ");
+        }
+    }
+
+
+}
