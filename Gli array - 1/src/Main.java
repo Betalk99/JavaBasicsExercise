33d8c5ecc4750beab475d59fb38057e38001cf31
Bruno Orlandi <bruno1999@hotmail.it>
2023-09-28 11:28:04 +0200

    add exercice Gli array - 1

--- Gli array - 1/src/Main.java
+++ Gli array - 1/src/Main.java
@@ -1,14 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        int[] arrayNum = creaERiempiNum();
 
-       System.out.println("La somma è : " + somma());
-    }
-    public static int somma(){
-        int[] num={1,2,3,4,5,6,7,8,9,10};
-        int sum=0;
-        for(int i=0;i<num.length;i++){
-            sum = sum + num[i];
+        for (int numero : arrayNum) {
+            System.out.print(numero + " ");
         }
-        return sum;
+
     }
+    public static int[] creaERiempiNum() {
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i+1;
+        }
+        return array;
+    }
+
+
+
+
+
+
 }
